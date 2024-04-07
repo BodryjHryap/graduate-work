f504f078c507ee0eb41fc4b58413021d72764fa9
Nikita <athlon64x2600@gmail.com>
2024-04-07 15:22:28 +0200

    app is working

---
+++ src/main/java/io/swagger/model/CreateOrUpdateComment.java
@@ -0,0 +1,80 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * CreateOrUpdateComment
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-04-07T11:59:23.949615055Z[GMT]")
+
+
+public class CreateOrUpdateComment   {
+  @JsonProperty("text")
+  private String text = null;
+
+  public CreateOrUpdateComment text(String text) {
+    this.text = text;
+    return this;
+  }
+
+  /**
+   * текст комментария
+   * @return text
+   **/
+  @Schema(required = true, description = "текст комментария")
+      @NotNull
+
+  @Size(min=8,max=64)   public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    CreateOrUpdateComment createOrUpdateComment = (CreateOrUpdateComment) o;
+    return Objects.equals(this.text, createOrUpdateComment.text);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(text);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class CreateOrUpdateComment {\n");
+    
+    sb.append("    text: ").append(toIndentedString(text)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
